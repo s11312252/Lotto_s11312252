7ddef30c561ae1f9747093fade9b756370cef9a2
student <lucyamy23780983@gmail.com>
2025-09-22 14:53:13 +0800

    第七次commit (螢幕觸控)(陳芯霈)

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -1,9 +1,11 @@
 package tw.edu.pu.csim.tcyang.lotto
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,7 +22,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.lotto.ui.theme.LottoTheme
 
-import androidx.compose.runtime.setValue // 引入 setValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 
 
 class MainActivity : ComponentActivity() {
@@ -41,15 +44,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Play(modifier: Modifier = Modifier) {
-    //var lucky = (1..100).random()
+    val context = LocalContext.current
     var lucky by remember {
         mutableStateOf((1..100).random())
     }
 
-    Column (modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-        ){
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .clickable { // <-- clickable 修飾符放在這裡
+                Toast.makeText(context, "螢幕觸控(陳芯霈)", Toast.LENGTH_SHORT).show()
+            },
+        verticalArrangement = Arrangement.Center, // <-- 參數被移到這裡
+        horizontalAlignment = Alignment.CenterHorizontally, // <-- 參數被移到這裡
+    ){
         Text(
             text = "樂透數字(1-100)為 $lucky"
         )
@@ -60,7 +68,4 @@ fun Play(modifier: Modifier = Modifier) {
             Text("重新產生樂透碼")
         }
     }
-
-
-
 }
